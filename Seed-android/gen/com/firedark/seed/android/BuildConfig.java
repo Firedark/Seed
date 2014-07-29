89a7001c5dfb6d4b0f3af06833ab0d9ff9a60f38
Firedark <firedarknes@gmail.com>
2014-07-29 15:46:58 +0200

    First Commit

---
+++ Seed-android/gen/com/firedark/seed/android/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.firedark.seed.android;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Seed-desktop/src/com/firedark/seed/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.firedark.seed.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.firedark.seed.Seed;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Seed(), config);
+	}
+}
